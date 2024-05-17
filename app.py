e17e4443a907a8742cad2de1041b5bf03d559bb4
Vo Ta Quang Nhat <votaquangnhat1104@gmail.com>
2024-05-17 11:53:15 +0700

    Change json file that API return

--- app.py
+++ app.py
@@ -10,8 +10,16 @@ es = Search()
 
 @app.get('/search/<size>/<from_>') #for java client take date
 def java_search_all(size, from_):
-    results, aggs = search(query='', size=size, from_=from_)
-    json_file = json.dumps(results['hits']['hits'], indent=4)
+    search_results, aggs = search(query='', size=size, from_=from_)
+    results = []
+    for document in search_results['hits']['hits']:
+        article = document['_source']
+        d = {}
+        d['id'] = document['_id']
+        d['titles'] = article['title']
+        results.append(d)
+        
+    json_file = json.dumps(results, indent=4)
     return Response(json_file, status=200, mimetype='application/json')
 
 
